4e3b073888c15bc52bdb23bcb1374912075f13f9
jithinsankar <jithinsankar.nk@gmail.com>
2024-03-29 01:27:47 +0530

    mdx setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import createMDX from "@next/mdx"
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
 
-export default nextConfig;
+const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+    options: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+  })
+
+export default withMDX(nextConfig)
