77bacebd3ed2162f45921087abce02a124b7f8d0
jithinsankar <jithinsankar.nk@gmail.com>
2024-03-29 11:41:22 +0530

    updated navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,34 @@
+import { Linkedin, LucideGithub, LucideTwitter } from "lucide-react";
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <div className="h-14 pt-10"></div>;
+  return <div className="">
+    <div className="top-0 h-14 w-full border-b-2">
+      <div className="max-w-screen-md mx-auto flex items-center p-4 justify-between">
+        <div className="flex space-x-4">
+        <div className="flex hover:opacity-75 transition items-center">
+            <Link href="/">Home</Link>
+          </div>
+          <div className="flex hover:opacity-75 transition items-center">
+            <Link href="/notes">Notes</Link>
+          </div>
+          <div className="flex hover:opacity-75 transition items-center">
+            <Link href="/#posts">Posts</Link>
+          </div>
+
+        </div>
+        <div className="flex space-x-4">
+        <div className="flex hover:opacity-75 transition items-center">
+            <Link href="https://www.linkedin.com/in/jithinsankarnk/"><Linkedin className="h-4 w-4"/></Link>
+          </div>
+          <div className="flex hover:opacity-75 transition items-center">
+            <Link href="https://twitter.com/jithins4nkar"><LucideTwitter className="h-4 w-4"/></Link>
+          </div>
+          <div className="flex hover:opacity-75 transition items-center">
+            <Link href="https://github.com/jithinsankar"><LucideGithub className="h-4 w-4"/></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,5 +2,5 @@ import { Navbar } from "../components/navbar";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     // Create any shared layout or styles here
-    return <div className="flex flex-col justify-center p-6 w-full m-auto max-w-screen-md"><Navbar/>{children}</div>
+    return <div className=""><Navbar/><div className="flex flex-col justify-center p-6 w-full m-auto max-w-screen-md prose dark:prose-invert">{children}</div></div>
   }
