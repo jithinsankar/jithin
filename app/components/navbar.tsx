be3ba6683964312bd047389cdf1f76309201f1a9
jithinsankar <jithinsankar.nk@gmail.com>
2024-03-29 12:50:59 +0530

    minor layout fix

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,9 +8,9 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return <div className="">
-    <div className="top-0 h-14 border-b-2 flex max-w-screen-md mx-auto ">
+    <div className="sticky z-10 top-0 h-14 border-b-2 flex max-w-screen-md mx-auto ">
       <div className="flex w-full items-center justify-between">
-        <div className="flex items-center space-x-4 ml-4">
+        <div className="flex items-center ml-4">
         <div className={clsx("flex hover:opacity-75 transition items-center  p-2 px-4 justify-center rounded-sm",{'bg-secondary text-primary': pathname === '/',})}>
             <Link href="/">Home</Link>
           </div>
@@ -22,16 +22,17 @@ export const Navbar = () => {
           </div>
 
         </div>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 mr-4">
+        <div className="flex hover:opacity-75 transition items-center">
+            <Link href="https://github.com/jithinsankar"><LucideGithub className="h-4 w-4"/></Link>
+          </div>
         <div className="flex hover:opacity-75 transition items-center">
             <Link href="https://www.linkedin.com/in/jithinsankarnk/"><Linkedin className="h-4 w-4" /></Link>
           </div>
           <div className="flex hover:opacity-75 transition items-center">
             <Link href="https://twitter.com/jithins4nkar"><LucideTwitter className="h-4 w-4"/></Link>
           </div>
-          <div className="flex hover:opacity-75 transition items-center">
-            <Link href="https://github.com/jithinsankar"><LucideGithub className="h-4 w-4"/></Link>
-          </div>
+
         </div>
       </div>
     </div>
